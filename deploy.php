02b0f1d5fc570b867e2aa32c46c92f2a49fbe5c4
msamgan <mohdsamgankhan@gmail.com>
2024-10-14 15:14:41 -0400

    added Deploy.

---
+++ deploy.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/msamgan/msamgan.dev.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('146.190.32.125')
+    ->set('remote_user', 'msamgan_dev')
+    ->set('deploy_path', '~/htdocs/msamgan.dev');
+
+desc('Build the assets');
+task('build', function () {
+    cd('{{release_path}}');
+    run('npm install');
+    run('npm run build');
+});
+
+task('optimize', function () {
+    cd('{{release_path}}');
+    run('php artisan optimize');
+});
+
+task('clear_cache', function () {
+    cd('{{current_path}}');
+    run('php artisan cache:clear');
+});
+
+after('deploy:update_code', 'build');
+after('deploy:symlink', 'optimize');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
