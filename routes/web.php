981ca2ea9496ce2aff8d9269a2ebd696f6f3ec55
msamgan <mohdsamgankhan@gmail.com>
2025-04-23 18:26:41 -0400

    udated the dashboard jsx with partials.

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,8 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => redirect('login'))->name('welcome');
+Route::inertia('dashboard', 'Dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-
-$moduleRoutesDir = 'routes/modules/';
-foreach (File::allFiles(base_path($moduleRoutesDir)) as $file) {
-    require base_path($moduleRoutesDir) . $file->getFilename();
-}
+autoloadRoutes();
