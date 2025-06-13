b3a2a250baa8dde5a79e25e8b08bcbb127733f09
msamgan <mohdsamgankhan@gmail.com>
2025-06-13 16:19:58 -0400

    update

--- resources/js/Components/dashboard/PostStats.jsx
+++ resources/js/Components/dashboard/PostStats.jsx
@@ -9,28 +9,14 @@ const PostStats = ({ publishedPosts, draftPosts }) => {
             <div className="mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-4">
                 <StatsCard
                     icon={
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512"
-                            fill="currentColor"
-                            className="h-9 w-9 text-gray-100"
-                        >
-                            <path d="M464,64H48A16,16,0,0,0,32,80V432a16,16,0,0,0,16,16H464a16,16,0,0,0,16-16V80A16,16,0,0,0,464,64ZM256,96a32,32,0,1,1-32,32A32,32,0,0,1,256,96Zm96,320H160V352H352Zm0-96H160V256H352Zm0-96H160V160H352Z"></path>
-                        </svg>
+                        <i className="ri-article-line text-gray-100 text-4xl"></i>
                     }
                     title="Published"
                     value={publishedPosts}
                 />
                 <StatsCard
                     icon={
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512"
-                            fill="currentColor"
-                            className="h-9 w-9 text-gray-100"
-                        >
-                            <path d="M464,64H48A16,16,0,0,0,32,80V432a16,16,0,0,0,16,16H464a16,16,0,0,0,16-16V80A16,16,0,0,0,464,64ZM256,96a32,32,0,1,1-32,32A32,32,0,0,1,256,96Zm96,320H160V352H352Zm0-96H160V256H352Zm0-96H160V160H352Z"></path>
-                        </svg>
+                        <i className="ri-draft-line text-gray-100 text-4xl"></i>
                     }
                     title="Draft"
                     value={draftPosts}
--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -39,15 +39,7 @@ export default function Login({ status, canResetPassword }) {
                     <InputLabel htmlFor="email" value="Email" required={true} />
                     <div className="relative">
                         <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 text-gray-400">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                            >
-                                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                            </svg>
+                            <i className="ri-mail-line text-sm"></i>
                         </div>
                         <TextInput
                             id="email"
@@ -78,18 +70,7 @@ export default function Login({ status, canResetPassword }) {
                     </div>
                     <div className="relative">
                         <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 text-gray-400">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                                    clipRule="evenodd"
-                                />
-                            </svg>
+                            <i className="ri-lock-line text-sm"></i>
                         </div>
                         <TextInput
                             id="password"
